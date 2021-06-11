b9dd23fa0ccd8dadb223676cd6202601fdbbb3e7
AhmedE686 <ahmedelnesr1999@gmail.com>
2021-06-11 08:01:31 -0700

    note

---
+++ main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  char x;
+  scanf("%d",&x);
+  if ((x>=0)&&(x<=49))
+  {
+      printf ("false");
+  }
+  else if((x>=50)&&(x<=64))
+  {
+           printf ("Pass");
+  }
+   else if((x>=65)&&(x<=74))
+  {
+           printf ("Good");
+  }
+   else if((x>=75)&&(x<=84))
+  {
+           printf ("Very.Good");
+  }
+  else if((x>=85)&&(x<=100)) // after change
+  {
+      printf("Excellent");
+  }
+  else
+    printf("Enter a valid number from 0 to 100 ");
+    return 0;
+}
